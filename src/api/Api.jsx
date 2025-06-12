5bef42661e2cc945ca044c0eec26b325f5920c15
Kurokenji <tthh38ht@gmail.com>
2025-06-12 18:54:23 +0700

    update api url

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,6 +2,8 @@
 
 // const BASE_URL = 'http://192.168.1.25:8000/api/';
 
-const BASE_URL = 'http://192.168.1.7:8000/api/';
+// const BASE_URL = 'http://192.168.1.7:8000/api/';
+ 
+const BASE_URL =  import.meta.env.VITE_API_BASE_URL;
 
 export default BASE_URL;
